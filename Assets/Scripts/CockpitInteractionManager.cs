248a009e50ed57baae7d6c1b67b95d8eff3a2e4e
Kritika Sharma <skritikaa563@gmail.com>
2026-08-18 10:19:26 +0530

    Add spacecraft cockpit and space environment

---
+++ Assets/Scripts/CockpitInteractionManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CockpitInteractionManager : MonoBehaviour
+{
+    public void TestConsoleInteraction()
+    {
+        Debug.Log("Cockpit console interaction received!");
+    }
+}
